ade0191d8fdcecd49161f79a30903a358e29bbb7
panshuxue <2291899411@qq.com>
2025-04-28 00:13:15 +0800

    feat: 包管理器配置提交

---
+++ scripts/preinstall.js
@@ -0,0 +1,8 @@
+// const npmExecPath = process.env.npm_execpath || ''
+// const isPnpm = npmExecPath.includes('pnpm')
+
+// if (!isPnpm) {
+//   console.warn('\x1b[33m此仓库必须使用 pnpm 作为包管理器。\x1b[0m')
+//   process.exit(1)
+// }
+console.log('111111')
